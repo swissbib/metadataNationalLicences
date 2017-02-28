fdecafbe3c2f97f1765836267e410cc98dd7b5fe
Lionel Walter <lionel.walter@unibas.ch>
2017-02-28 15:20:23 +0100

    script to transform all springer content

--- shell/full_process_springer.sh
+++ shell/full_process_springer.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 # local
-#DATA_BASE_DIR=/media/lionel/Data/swissbib-data/nationallizenzen/test-full/
-#CODE_BASE_DIR=/home/lionel/Documents/mycloud/swissbib/git_repo/metadataNationalLicences/
-#LOG_DIR=/media/lionel/Data/swissbib-data/nationallizenzen/test-full/log/
-
+DATA_BASE_DIR=/media/lionel/Data/swissbib-data/nationallizenzen/
+CODE_BASE_DIR=/home/lionel/Documents/mycloud/swissbib/git_repo/metadataNationalLicences/
+LOG_DIR=/media/lionel/Data/swissbib-data/nationallizenzen/test-full/log/
 
 # sb-coai2
-DATA_BASE_DIR=/swissbib/harvesting/nationalLicencesData/
-CODE_BASE_DIR=/swissbib/harvesting/nationalLicences/
-LOG_DIR=/swissbib/harvesting/nationalLicencesLog/
+#DATA_BASE_DIR=/swissbib/harvesting/nationalLicencesData/
+#CODE_BASE_DIR=/swissbib/harvesting/nationalLicences/
+#LOG_DIR=/swissbib/harvesting/nationalLicencesLog/
 
 
 DATE=`date +%Y-%m-%d:%H:%M:%S`
 
-PUBLISHER=oxford
+PUBLISHER=springer
 
 LOG_FILE=$LOG_DIR/$PUBLISHER-$DATE.log
 ERR_FILE=$LOG_DIR/$PUBLISHER-$DATE.err
 ERROR_VALIDATION_FILE=$LOG_DIR/$PUBLISHER-$DATE-error-validation.txt
 
 SRC_DIR=$DATA_BASE_DIR/$PUBLISHER/source/
-EXTRACTED_DIR=$DATA_BASE_DIR/$PUBLISHER/extracted/
+EXTRACTED_DIR=$DATA_BASE_DIR/$PUBLISHER/extracted/JOU=11543
 SWISSBIB_JATS_DIR=$DATA_BASE_DIR/$PUBLISHER/swissbib-jats/
 XSLT_DIRECTORY=$CODE_BASE_DIR/xslt/
 DTD_DIRECTORY=$CODE_BASE_DIR/dtd/
@@ -34,6 +33,6 @@ MERGED_DIR=$DATA_BASE_DIR/$PUBLISHER/merged/
 #./extract_oxford2.sh $SRC_DIR/delivery_2016_09_07/ $EXTRACTED_DIR >> $LOG_FILE 2>> $ERR_FILE
 
 
-./transform_records_oxford.sh $EXTRACTED_DIR $SWISSBIB_JATS_DIR $XSLT_DIRECTORY $DTD_DIRECTORY $ERROR_VALIDATION_FILE >> $LOG_FILE 2>> $ERR_FILE
-./merge_records_oxford.sh $SWISSBIB_JATS_DIR $MERGED_DIR >> $LOG_FILE 2>> $ERR_FILE
+./transform_records_springer.sh $EXTRACTED_DIR $SWISSBIB_JATS_DIR $XSLT_DIRECTORY $DTD_DIRECTORY $ERROR_VALIDATION_FILE >> $LOG_FILE 2>> $ERR_FILE
+#./merge_records_oxford.sh $SWISSBIB_JATS_DIR $MERGED_DIR >> $LOG_FILE 2>> $ERR_FILE
 
