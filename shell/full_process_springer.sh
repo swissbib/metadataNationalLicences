556e82563e5f09a26108370b98467e6430e08ce5
Lionel Walter <lionel.walter@unibas.ch>
2017-03-13 09:57:14 +0100

    run springer extraction scripts on sb-coai2

--- shell/full_process_springer.sh
+++ shell/full_process_springer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # local
-DATA_BASE_DIR=/media/lionel/Data/swissbib-data/nationallizenzen/
-CODE_BASE_DIR=/home/lionel/Documents/mycloud/swissbib/git_repo/metadataNationalLicences/
-LOG_DIR=/media/lionel/Data/swissbib-data/nationallizenzen/test-full/log/
+#DATA_BASE_DIR=/media/lionel/Data/swissbib-data/nationallizenzen/
+#CODE_BASE_DIR=/home/lionel/Documents/mycloud/swissbib/git_repo/metadataNationalLicences/
+#LOG_DIR=/media/lionel/Data/swissbib-data/nationallizenzen/test-full/log/
 
 # sb-coai2
-#DATA_BASE_DIR=/swissbib/harvesting/nationalLicencesData/
-#CODE_BASE_DIR=/swissbib/harvesting/nationalLicences/
-#LOG_DIR=/swissbib/harvesting/nationalLicencesLog/
+DATA_BASE_DIR=/swissbib/harvesting/nationalLicencesData/
+CODE_BASE_DIR=/swissbib/harvesting/nationalLicences/
+LOG_DIR=/swissbib/harvesting/nationalLicencesLog/
 
 
 DATE=`date +%Y-%m-%d:%H:%M:%S`
@@ -29,6 +29,6 @@ MERGED_DIR=$DATA_BASE_DIR/$PUBLISHER/merged/
 
 
 ./extract_springer.sh $SRC_DIR/ $EXTRACTED_DIR >> $LOG_FILE 2>> $ERR_FILE
-./transform_records_springer.sh $EXTRACTED_DIR $SWISSBIB_JATS_DIR $XSLT_DIRECTORY $DTD_DIRECTORY $ERROR_VALIDATION_FILE >> $LOG_FILE 2>> $ERR_FILE
+#./transform_records_springer.sh $EXTRACTED_DIR $SWISSBIB_JATS_DIR $XSLT_DIRECTORY $DTD_DIRECTORY $ERROR_VALIDATION_FILE >> $LOG_FILE 2>> $ERR_FILE
 #./merge_records_oxford.sh $SWISSBIB_JATS_DIR $MERGED_DIR >> $LOG_FILE 2>> $ERR_FILE
 
