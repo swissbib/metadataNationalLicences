aed8a6ad5e99d6ed12b0fe4b1ded84ee7eb82b51
Lionel Walter <lionel.walter@unibas.ch>
2018-06-19 13:23:42 +0200

    regenerate title lists with rerodoc url

--- python/checkFulltextExists.py
+++ python/checkFulltextExists.py
@@ -3,11 +3,12 @@ import unicodecsv as csv
 
 files=[]
 
-with open("../swiss-publications-lists/rerodoc.csv") as csvfile:
+with open("../swiss-publications-lists-springer/rerodoc.csv") as csvfile:
     publications = csv.reader(csvfile, dialect='excel')
     for row in publications:
         path=row[1]
+        path='/media/lionel/Data/swissbib-data/nationallizenzen/springer/swiss-pdf/'+path
         id=row[0]
-        doi=row[0][24:]
+        doi=row[0][26:]
         if not(os.path.isfile(path)):
             print "missing," + doi + ","  + path + "," + id
